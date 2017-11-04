37109844897fb107d77bf4577473b648d10103c2
Panfeng <qidaizhe11@gmail.com>
2017-11-04 20:05:05 +0800

    cart.js，补充State类型声明

--- src/store/modules/cart.ts
+++ src/store/modules/cart.ts
@@ -1,16 +1,26 @@
 import shop from '../../api/shop'
 import * as types from '../mutation-types'
 
+interface Shape {
+  id: number
+  quantity: number
+}
+
+export interface State {
+  added: Shape[]
+  checkoutStatus: 'successful' | 'failed' | null
+}
+
 // initial state
 // shape: [{ id, quantity }]
-const state = {
+const state: State = {
   added: [],
   checkoutStatus: null
 }
 
 // getters
 const getters = {
-  checkoutStatus: state => state.checkoutStatus
+  checkoutStatus: (state: State) => state.checkoutStatus
 }
 
 // actions
@@ -28,7 +38,7 @@ const actions = {
 
 // mutations
 const mutations = {
-  [types.ADD_TO_CART] (state, { id }) {
+  [types.ADD_TO_CART] (state: State, { id }) {
     state.checkoutStatus = null
     const record = state.added.find(p => p.id === id)
     if (!record) {
@@ -41,17 +51,17 @@ const mutations = {
     }
   },
 
-  [types.CHECKOUT_REQUEST] (state) {
+  [types.CHECKOUT_REQUEST] (state: State) {
     // clear cart
     state.added = []
     state.checkoutStatus = null
   },
 
-  [types.CHECKOUT_SUCCESS] (state) {
+  [types.CHECKOUT_SUCCESS] (state: State) {
     state.checkoutStatus = 'successful'
   },
 
-  [types.CHECKOUT_FAILURE] (state, { savedCartItems }) {
+  [types.CHECKOUT_FAILURE] (state: State, { savedCartItems }) {
     // rollback to the cart saved before sending the request
     state.added = savedCartItems
     state.checkoutStatus = 'failed'
