a6fd027ae236d8838944144995e2fc9853adaf4a
Panfeng <qidaizhe11@gmail.com>
2017-11-04 19:52:10 +0800

    currency.js，typescript改造

--- src/currency.ts
+++ src/currency.ts
@@ -1,22 +1,22 @@
 const digitsRE = /(\d{3})(?=\d)/g
 
-export function currency (value, currency, decimals) {
+export function currency (value: any, currency: any, decimals: any) {
   value = parseFloat(value)
   if (!isFinite(value) || (!value && value !== 0)) return ''
   currency = currency != null ? currency : '$'
   decimals = decimals != null ? decimals : 2
-  var stringified = Math.abs(value).toFixed(decimals)
-  var _int = decimals
+  const stringified = Math.abs(value).toFixed(decimals)
+  const _int = decimals
     ? stringified.slice(0, -1 - decimals)
     : stringified
-  var i = _int.length % 3
-  var head = i > 0
+  const i = _int.length % 3
+  const head = i > 0
     ? (_int.slice(0, i) + (_int.length > 3 ? ',' : ''))
     : ''
-  var _float = decimals
+  const _float = decimals
     ? stringified.slice(-1 - decimals)
     : ''
-  var sign = value < 0 ? '-' : ''
+  const sign = value < 0 ? '-' : ''
   return sign + currency + head +
     _int.slice(i).replace(digitsRE, '$1,') +
     _float
