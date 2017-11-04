ae5148ab5a8da002b533196939d0223c2392d3c9
Panfeng <qidaizhe11@gmail.com>
2017-11-04 19:56:44 +0800

    api/shop.js，基于any的简单ts改造

--- src/api/shop.ts
+++ src/api/shop.ts
@@ -2,17 +2,17 @@
  * Mocking client-server processing
  */
 const _products = [
-  {"id": 1, "title": "iPad 4 Mini", "price": 500.01, "inventory": 2},
-  {"id": 2, "title": "H&M T-Shirt White", "price": 10.99, "inventory": 10},
-  {"id": 3, "title": "Charli XCX - Sucker CD", "price": 19.99, "inventory": 5}
+  { 'id': 1, 'title': 'iPad 4 Mini', 'price': 500.01, 'inventory': 2 },
+  { 'id': 2, 'title': 'H&M T-Shirt White', 'price': 10.99, 'inventory': 10 },
+  { 'id': 3, 'title': 'Charli XCX - Sucker CD', 'price': 19.99, 'inventory': 5 }
 ]
 
 export default {
-  getProducts (cb) {
+  getProducts (cb: any) {
     setTimeout(() => cb(_products), 100)
   },
 
-  buyProducts (products, cb, errorCb) {
+  buyProducts (products: any, cb: any, errorCb: any) {
     setTimeout(() => {
       // simulate random checkout failure.
       (Math.random() > 0.5 || navigator.userAgent.indexOf('PhantomJS') > -1)
