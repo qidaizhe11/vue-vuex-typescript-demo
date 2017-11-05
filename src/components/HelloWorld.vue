9e7d1c514304fcc3d77bee4a038edbc6e45c2786
Panfeng <qidaizhe11@gmail.com>
2017-11-05 15:55:10 +0800

    HelloWorld.vue，显示清理，只显示vuex示例链接

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,22 +3,6 @@
     <h1>{{ msg }}</h1>
     <h2>Vuex</h2>
     <router-link to="/vuex">ShoppingCart Example</router-link>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
   </div>
 </template>
 
