904daa7128b56685775a33c967e3e19e70a59dd3
Panfeng <qidaizhe11@gmail.com>
2017-12-05 12:12:36 +0800

    跟随最新版vuejs-templates/webpack生成

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,11 +17,9 @@ module.exports = {
   ],
   // add your custom rules here
   'rules': {
-    // allow paren-less arrow functions
-    'arrow-parens': 0,
     // allow async-await
-    'generator-star-spacing': 0,
+    'generator-star-spacing': 'off',
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   }
 }
